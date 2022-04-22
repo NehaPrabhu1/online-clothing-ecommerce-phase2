59f7686ca1b8f5d5d7293671f7e7116d5abb8d61
Neha Prabhu <nehagprabhu1@gmail.com>
2022-04-22 20:08:56 +0530

    Spring Boot Application

---
+++ ecommerce-online-clothing/src/main/java/com/onlineclothing/springboot/EcommerceOnlineClothingApplication.java
@@ -0,0 +1,13 @@
+package com.onlineclothing.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceOnlineClothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceOnlineClothingApplication.class, args);
+	}
+
+}
---
+++ ecommerce-online-clothing/src/test/java/com/onlineclothing/springboot/EcommerceOnlineClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineclothing.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOnlineClothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
